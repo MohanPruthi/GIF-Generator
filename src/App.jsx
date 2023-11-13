9aeabc85c299f078a54d700641d2cda6d515f9ea
Sahil Sajwan <cls>
2023-11-13 12:07:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
       <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
        px-10 py-2 text-4xl font-bold ">
-      RANDOM GIFS</h1><div>hello</div>
+      RANDOM GIFS</h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
         <Random />
         <Tag />
