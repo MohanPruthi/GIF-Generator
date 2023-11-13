758f0086e86328b74264f12da55566480c2b18fc
Sahil Sajwan <cls>
2023-11-13 11:56:39 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Tag from "./components/Tag";
 export default function App() {
   return (
 
+
+    //Second Commit
     <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
       <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
        px-10 py-2 text-4xl font-bold ">
